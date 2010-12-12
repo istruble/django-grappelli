a65c703f2d7a20c889bd356e795358b7a48281cb
Ian Struble <istruble@gmail.com>
2010-12-12 10:21:23 -0800

    Yet another pass at autocomplete.
    Turned code from http://code.djangoproject.com/ticket/14370
    into stand alone files instead of updates to the django core.
    
    Remove a 'overflow: hidden' rule in grappelli css.
    
    Basic usage:
    
        from grappelli.options import AutoCompleteModelAdmin
    
        class YourAdmin(AutoCompleteModelAdmin):
            # ...
            autocomplete_fields = {
                'user': { 'fields': ('username',), },
            }

---
+++ grappelli/options.py
@@ -0,0 +1,109 @@
+import operator
+
+from django.contrib import admin
+from django.db import models
+from django.db.models.query import QuerySet
+from django.http import HttpResponse, HttpResponseNotFound
+from django.utils import simplejson
+from django.utils.encoding import smart_str
+from grappelli.widgets import AutocompleteWidget, MultipleAutocompleteWidget
+
+
+AUTOCOMPLETE_FIELDS_DEFAULTS = {
+    'id': 'pk',
+    'limit': 5,
+    'value': lambda o: unicode(o),
+    'label': lambda o: unicode(o),
+}
+
+class AutoCompleteModelAdmin(admin.ModelAdmin):
+    """
+    Based on http://code.djangoproject.com/ticket/14370 and 
+    the changeset in it.  
+
+    http://bitbucket.org/tyrion/django/changeset/04488ec05e92
+    """
+    autocomplete_fields = {}
+
+    def __init__(self, *args, **kwargs):
+        super(AutoCompleteModelAdmin, self).__init__(*args, **kwargs)
+
+        def build_setting(value):
+            if value in settings['queryset'].model._meta.get_all_field_names():
+                return lambda m: getattr(m, value)
+            return lambda m: value % vars(m)
+
+        autocomplete_fields = {}
+        for (field, values) in self.autocomplete_fields.items():
+            settings = autocomplete_fields[field] = AUTOCOMPLETE_FIELDS_DEFAULTS.copy()
+            settings.update(values)
+            if hasattr(self.model, field):
+                rel = getattr(self.model, field).field.rel
+                settings['id'] = settings.get('id', rel.get_related_field().name)
+                if not settings.get('queryset'):
+                    settings['queryset'] = rel.to._default_manager.complex_filter(rel.limit_choices_to)
+            for option in ('value', 'label'):
+                if isinstance(settings[option], (str, unicode)):
+                    settings[option] = build_setting(settings[option])
+
+        self.autocomplete_fields = autocomplete_fields
+
+    def get_urls(self):
+        from django.conf.urls.defaults import patterns, url
+        urlpatterns = patterns('',
+            url(r'^autocomplete/(?P<field>[\w]+)/$',
+                self.autocomplete),
+        )
+        return urlpatterns + super(AutoCompleteModelAdmin, self).get_urls()
+
+    def autocomplete(self, request, field, extra_content=None):
+        query = request.GET.get('term', None)
+
+        if field not in self.autocomplete_fields or query is None:
+            raise Http404
+
+        settings = self.autocomplete_fields[field]
+        queryset = settings['queryset']
+
+        def construct_search(field_name):
+            # use different lookup methods depending on the notation
+            if field_name.startswith('^'):
+                return "%s__istartswith" % field_name[1:]
+            elif field_name.startswith('='):
+                return "%s__iexact" % field_name[1:]
+            elif field_name.startswith('@'):
+                return "%s__search" % field_name[1:]
+            else:
+                return "%s__icontains" % field_name
+
+        for bit in query.split():
+            or_queries = [models.Q(**{construct_search(
+                            smart_str(field_name)): bit})
+                          for field_name in settings['fields']]
+            queryset = queryset.filter(reduce(operator.or_, or_queries))
+
+        data = []
+        for o in queryset[:settings['limit']]:
+            data.append({
+                    'id': getattr(o, settings['id']),
+                    'value': settings['value'](o),
+                    'label': settings['label'](o),
+                    })
+        return HttpResponse(simplejson.dumps(data))
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        """
+        Overrides the default widget for ManyToMany & ForeignKey fields 
+        if they are specified in the autocomplete_fields class attribute.
+        """
+        db = kwargs.get('using')
+        if isinstance(db_field, models.ManyToManyField) and \
+                db_field.name in self.autocomplete_fields:
+            kwargs['widget'] = MultipleAutocompleteWidget(
+                self.autocomplete_fields[db_field.name], using=db)
+            kwargs['help_text'] = ''
+        elif isinstance(db_field, models.ForeignKey) and \
+                db_field.name in self.autocomplete_fields:
+            kwargs['widget']= AutocompleteWidget(
+                self.autocomplete_fields[db_field.name], using=db)
+        return super(AutoCompleteModelAdmin, self).formfield_for_dbfield(db_field, **kwargs)
---
+++ grappelli/widgets.py
@@ -0,0 +1,95 @@
+from django import forms
+from django.conf import settings
+from django.forms.util import flatatt
+from django.utils import simplejson
+from django.utils.safestring import mark_safe
+from django.utils.text import truncate_words
+
+#from django.utils.html import escape
+
+class AutocompleteWidget(forms.Widget):
+    class Media:
+        css = {
+            'all': ( 
+                settings.ADMIN_MEDIA_PREFIX+'css/jquery-ui-grappelli-extensions.css',
+                ),
+        }
+        js = (
+            settings.ADMIN_MEDIA_PREFIX+'js/autocomplete.js',
+        )
+
+    def __init__(self, settings, attrs=None, using=None, **js_options):
+        self.settings = settings
+        self.db = using
+        self.js_options = {
+            'source': settings.get('source'),
+            'multiple': settings.get('multiple', False),
+            'force_selection': settings.get('force_selection', True),
+            }
+        self.js_options.update(js_options)
+        super(AutocompleteWidget, self).__init__(attrs)
+
+    def get_autocomplete_url(self, name):
+        return '../autocomplete/%s/' % name
+
+    def render(self, name, value, attrs=None, hattrs=None, initial_objects=u''):
+        if value is None:
+            value = ''
+        hidden_id = 'id_hidden_%s' % name
+        hidden_attrs = self.build_attrs(type='hidden', name=name, value=value, id=hidden_id)
+        normal_attrs = self.build_attrs(attrs, type='text')
+        if value:
+            normal_attrs['value'] = self.label_for_value(value)
+        if not self.js_options.get('source'):
+            self.js_options['source'] = self.get_autocomplete_url(name)
+        options = simplejson.dumps(self.js_options)
+        return mark_safe(u'\n'.join((
+            u'<input%s />\n' % flatatt(hidden_attrs),
+            u'<input%s />\n' % flatatt(normal_attrs),
+            initial_objects,
+            u'<script type="text/javascript">',
+            u'django.jQuery("#id_%s").djangoautocomplete(%s);' % (name, options),
+            u'</script>\n',
+            )))
+
+    def label_for_value(self, value):
+        qs, key, value_fmt = [self.settings[k] for k in ('queryset', 'id', 'value')]
+        try:
+            obj = qs.get(**{key: value})
+            return value_fmt(obj)
+        except qs.model.DoesNotExist:
+            # should never happen.
+            return value
+
+class MultipleAutocompleteWidget(AutocompleteWidget):
+
+    def __init__(self, settings, attrs=None, using=None, **js_options):
+        js_options['multiple'] = True
+        super(MultipleAutocompleteWidget, self).__init__(settings, attrs,
+            using, **js_options)
+
+    def render(self, name, value, attrs=None, hattrs=None):
+        if value:
+            initial_objects = self.initial_objects(value)
+            value = ','.join([str(v) for v in value])
+        else:
+            initial_objects = u''
+        return super(MultipleAutocompleteWidget, self).render(name, value, 
+            attrs, hattrs, initial_objects)
+
+    def label_for_value(self, value):
+        return ''
+
+    def value_from_datadict(self, data, files, name):
+        value = data.get(name)
+        if value:
+            return value.split(',')
+        return value
+
+    def initial_objects(self, value):
+        qs, key, label_fmt = [self.settings[k] for k in ('queryset','id','label')]
+        output = [u'<ul class="ui-autocomplete-values">']
+        for obj in qs.filter(**{'%s__in' % key: value}):
+            output.append(u'<li>%s</li>' % label_fmt(obj))
+        output.append(u'</ul>\n')
+        return mark_safe(u'\n'.join(output))
